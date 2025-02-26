f8de19cea4f41dc132374be5d8ad0c80acd130fd
Ujjwal Taneja <ujjwal00031@gmail.com>
2025-02-26 19:19:18 +0530

    small issues resolved

--- first.js
+++ first.js
@@ -2,7 +2,7 @@ let qrimg = document.getElementById('qrImage');
 let imgBox = document.getElementById('imgBox');
 let qrText = document.getElementById('qrText');
 function genrateQR(){
-  if(qrText.value.length > 0){
+  if(qrText.value.length > 0 && qrText.value.trim() !== ''){
 qrimg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + qrText.value;
 imgBox.classList.add('show-img');
 } else{
